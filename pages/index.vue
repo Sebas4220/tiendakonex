186bc3ea8f9148a69136118238241cad837b305a
Sebas4220 <106259308+Sebas4220@users.noreply.github.com>
2026-07-01 14:33:29 -0400

    Add files via upload

--- pages/index.vue
+++ pages/index.vue
@@ -1063,20 +1063,14 @@
 
           <!-- Sección: Método de Pago -->
           <div class="border-top pt-3 mb-3">
-            <h6
-              class="fw-bold mb-2 py-2 text-dark d-flex align-items-center gap-1.5"
-              style="font-size: 0.9rem"
-            >
-              <i
-                class="bi bi-credit-card-fill text-muted me-1"
-                style="font-size: 0.95rem"
-              ></i>
-              Método de Pago
-            </h6>
             <div class="row g-2">
               <div class="col-12">
-                <div class="mb-3">
+                <div class="mb-1">
                   <label for="metodoPago" class="form-label fw-bold text-dark small mb-2">
+                    <i
+                      class="bi bi-credit-card-fill text-muted me-1"
+                      style="font-size: 0.95rem"
+                    ></i>
                     Forma de Pago
                   </label>
                   <select
